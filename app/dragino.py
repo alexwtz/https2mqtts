741a657c11a2b72485825fa85c43fd61c46a4a76
Alexandre Wetzel <alexwtz@gmail.com>
2024-02-29 22:41:51 +0100

    ajout output log

--- app/dragino.py
+++ app/dragino.py
@@ -40,17 +40,19 @@ def decode_lht65(bytes):
     }
 
 def decode_body(body):
-    # Decode the byte string to a regular string
-    json_string = body.decode('utf-8')
+    if not body:
+        # Decode the byte string to a regular string
+        json_string = body.decode('utf-8')
 
-    # Parse the JSON string into a Python dictionary
-    json_data = json.loads(json_string)
-    
-    # Convert the base64-encoded string to bytes
-    base64_string = json_data.get('payload')
-    bytes_data = base64.b64decode(base64_string)
-    json_data['decoded']= decode_lht65(bytes_data)
-    return json_data
+        # Parse the JSON string into a Python dictionary
+        json_data = json.loads(json_string)
+        
+        # Convert the base64-encoded string to bytes
+        base64_string = json_data.get('payload')
+        bytes_data = base64.b64decode(base64_string)
+        json_data['decoded']= decode_lht65(bytes_data)
+        return json_data
+    return body
 
 def main():
     body = b'{\n  "id": "c9ac2be0-357a-4432-a7f8-5b1991489a95",\n  "name": "Dragino LHT65",\n  "app_eui": "A000000000000101",\n  "dev_eui": "A840413BD182D3DC",\n  "devaddr": "11000048",\n  "downlink_url": "https://console.helium.com/api/v1/down/8bb0ff63-f6b1-43a6-9272-7145376ddb9f/zRFYJ40npC3K6EesgBAv9w0Nfz8B_SMF/c9ac2be0-357a-4432-a7f8-5b1991489a95",\n  "fcnt": "511",\n  "port": "2",\n  "payload": "y8QHKwJtAQcOf/8=",\n  "reported_at": "1709239250688"\n}'
